c26a58c02f957f0dc80132fcc5d27c1beabc7aab
annabelleg <agary110@gmail.com>
2015-01-12 13:16:56 -0500

    Create GetName.java

---
+++ GetName.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class GetName{
+    private int unusual;
+
+    public GetName(String gender, int u){
+	csv = chooseCSV(gender);
+	unusual = u;
+    }
+    public String chooseCSV(String g){
+	csv = "";
+	if (gender = "male" || gender = "both"){
+	    csv = "boys.csv";
+	}else if (gender = "female"){
+	    csv = "girls.csv";
+	}
+	return csv;
+  }
+
+    public static void main(String[]args) throws IOException{
+	
+    }
+} //we need to put something from the CSV into the GUI
