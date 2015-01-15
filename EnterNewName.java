3305c079a13c725261391de4b1ab0a92ad8a074f
annabelleg <agary110@gmail.com>
2015-01-14 19:34:18 -0500

    add AddName(), added oldfashioned buttons

--- EnterNewName.java
+++ EnterNewName.java
@@ -2,13 +2,14 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.io.*;
 
 public class EnterNewName extends JFrame implements ActionListener{
     private Container pane;
-    private JTextArea text, text2, text3, text4;
+    private JTextArea text, text2, text3, text4, text5, text6;
     private JButton add;
     private JTextField name;
-    private ButtonGroup gender, unusual;
+    private ButtonGroup gender, unusual, oldfashioned;
 
     public EnterNewName(){
 	this.setTitle("Aaron and Annabelle's Name Generator!");
@@ -76,17 +77,58 @@ public class EnterNewName extends JFrame implements ActionListener{
         radioPanel2.add(Button9);
         radioPanel2.add(Button10);
 
+	JRadioButton B1 = new JRadioButton("1");
+	JRadioButton B2 = new JRadioButton("2");
+	JRadioButton B3 = new JRadioButton("3");
+	JRadioButton B4 = new JRadioButton("4");
+	JRadioButton B5 = new JRadioButton("5");
+	Button5.setSelected(true);
+	JRadioButton B6 = new JRadioButton("6");
+	JRadioButton B7 = new JRadioButton("7");
+	JRadioButton B8 = new JRadioButton("8");
+	JRadioButton B9 = new JRadioButton("9");
+	JRadioButton B10 = new JRadioButton("10");
+	oldfashioned = new ButtonGroup();
+	oldfashioned.add(B1);
+	oldfashioned.add(B2);
+	oldfashioned.add(B3);
+	oldfashioned.add(B4);
+	oldfashioned.add(B5);
+	oldfashioned.add(B6);
+	oldfashioned.add(B7);
+	oldfashioned.add(B8);
+	oldfashioned.add(B9);
+	oldfashioned.add(B10);
+	JPanel radioPanel3 = new JPanel(new GridLayout(1, 0));
+        radioPanel3.add(B1);
+        radioPanel3.add(B2);
+        radioPanel3.add(B3);
+        radioPanel3.add(B4);
+        radioPanel3.add(B5);
+        radioPanel3.add(B6);
+        radioPanel3.add(B7);
+        radioPanel3.add(B8);
+        radioPanel3.add(B9);
+        radioPanel3.add(B10);
+
 	text2 = new JTextArea("Is it a boy's name, a girl's name, or both.");
 	text2.setEditable(false);
 	pane.add(text2);
 	add(radioPanel, BorderLayout.LINE_START);
-	text3 = new JTextArea("Rate how unusual the name is from 1 to 10");
+	text3 = new JTextArea("Rate how unusual the name is from 1 to 10,\nwith 10 being the most unusual");
 	text3.setEditable(false);
 	pane.add(text3);
-	text4 = new JTextArea("E.X. Mary would be rated a 1, Ximena would be rated a 10");
+	text4 = new JTextArea("Ex. Mary would be rated a 1, Ximena would be rated a 10");
 	text4.setEditable(false);
 	pane.add(text4);
 	add(radioPanel2, BorderLayout.LINE_START);
+	text5 = new JTextArea("Rate how old-fashioned the name is from 1 to 10,\nwith 10 being the most old-fashioned");
+	text5.setEditable(false);
+	pane.add(text5);
+	text6 = new JTextArea("Ex. Mary would be rated a 10, Ximena would be rated a 1");
+	text6.setEditable(false);
+	pane.add(text6);
+	add(radioPanel3, BorderLayout.LINE_START);
 	
 	add = new JButton("Add Name!");
 	pane.add(add);
@@ -94,19 +136,7 @@ public class EnterNewName extends JFrame implements ActionListener{
 	add.addActionListener(this);
 	
     }
-    public Person collectData(){
-	Person p = new Person(name.getText(), buttonVal(unusual));
-	String csv = "";
-	if (buttonVal(gender).equals("Boy")) csv = "boys.csv";
-	if (buttonVal(gender).equals("Girl")) csv = "girls.csv";
-	if (buttonVal(gender).equals("Both")) csv = "both";
-    }
-    public void actionPerformed(ActionEvent e){
-	String action = e.getActionCommand();
-	if (action.equals("go")){
-	    
-	}
-    }  
+
     public String buttonVal(ButtonGroup b){
 	for (Enumeration<AbstractButton> buttons = b.getElements(); buttons.hasMoreElements();){
 	    AbstractButton button = buttons.nextElement();
@@ -116,6 +146,35 @@ public class EnterNewName extends JFrame implements ActionListener{
 	}
 	return null;
     }
+
+    public void collectData() throws IOException{
+	Person p = new Person(name.getText(), buttonVal(unusual), buttonVal(oldfashioned));
+	String csv = "";
+	if (buttonVal(gender).equals("Boy")) csv = "boys.csv";
+	if (buttonVal(gender).equals("Girl")) csv = "girls.csv";
+	if (buttonVal(gender).equals("Both")) csv = "both";
+	AddName(p, csv);
+    }
+
+    public void AddName(Person p, String csv) throws IOException{
+	String content = p.qualities();
+	if (csv.equals("boys.csv") || csv.equals("girls.csv")){
+	    FileWriter w = new FileWriter(csv, true);
+	    w.write(content);
+	    w.close();
+	} else {
+	    AddName(p,"boys.csv");
+	    AddName(p,"girls.csv");
+	}
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String action = e.getActionCommand();
+	if (action.equals("go")){
+	    
+	}
+    }  
+
     public static void main(String[]args){
 	EnterNewName g = new EnterNewName();
 	g.setVisible(true);
