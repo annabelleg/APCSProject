f1fbf3223e26963b55485613f4c05f7b6e12a16e
annabelleg <agary110@gmail.com>
2015-01-18 23:49:11 -0500

    made it work better/look prettier

--- Error.java
+++ Error.java
@@ -8,14 +8,15 @@ public class Error extends JFrame{
 
     public Error(){
 	this.setTitle("Sorry!");
-	this.setSize(300,200);
+	this.setSize(275,80);
 	this.setLocation(300,150);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
 	
 	text = new JTextArea("Sorry, that name is already in our system! \nPlease close this window \nand try again with a new name.");
+	text.setEditable(false);
 	pane.add(text);
     }
     public static void main(String[]args){
